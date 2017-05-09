63bed12c5004ef58df8d5e622de0fc1df47aca35
Andrew Turley <andrew.turley@achilles.com>
2017-05-09 10:45:16 +0100

    Add reducer and start redux

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -2,9 +2,12 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducer = (state = {name: 'Anonymouse'}, action) => {
-
-
+var stateDefault = {
+    searchText: '',
+    showCompleted: false,
+    todos: []
+};
+var reducer = (state = stateDefault, action) => {
     return state;
 };
 
