bc910a4c135be0c5935aeed9bdae38c8b39dbca1
Andrew Turley <andrew.turley@achilles.com>
2017-05-09 11:06:07 +0100

    subscribers

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -20,13 +20,26 @@ var reducer = (state = stateDefault, action) => {
     return state;
 };
 
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, redux.compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 
-var currentState = store.getState();
-console.log('currentState', currentState);
+// subscribe to changes
+
+var unsubscribe = store.subscribe(() => {
+    var state = store.getState();
+
+    document.getElementById('app').innerHTML = state.searchText;
+});
 
 store.dispatch({
     type:'CHANGE_SEARCH_TEXT',
     searchText: 'work'
 });
-console.log('searchText should be work', store.getState());
+
+
+
+store.dispatch({
+    type:'CHANGE_SEARCH_TEXT',
+    searchText: 'time'
+});
